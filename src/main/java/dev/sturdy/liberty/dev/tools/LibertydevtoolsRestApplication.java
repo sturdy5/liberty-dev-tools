fe558d7fe0811c2c51227a3f43034af79aa457df
Sturdy5 <1645884+sturdy5@users.noreply.github.com>
2023-08-31 21:27:59 -0400

    As is from the starter

---
+++ src/main/java/dev/sturdy/liberty/dev/tools/LibertydevtoolsRestApplication.java
@@ -0,0 +1,11 @@
+package dev.sturdy.liberty.dev.tools;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class LibertydevtoolsRestApplication extends Application {
+}
